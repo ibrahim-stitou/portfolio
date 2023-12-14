521d49f020845f67750bc7b10bf44e3617513bae
ibrahim stitou <stitou.brahim.stitou@gmail.com>
2023-12-14 15:27:44 +0100

    first commit

---
+++ src/components/Img.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Img() {
+  return (
+    <div>
+      <div>
+        <img
+          src="pp.jpg"
+          style={{ borderRadius: "50%", width: "100px", height: "100px" }}
+        />
+      </div>
+    </div>
+  );
+}
